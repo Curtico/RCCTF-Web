13b626683ec982c41d4228403269bff4711c8b25
Curtice Gough <31411535+Curtico@users.noreply.github.com>
2022-06-30 13:32:41 -0400

    Removed unnecessary 'getCookie()' function

--- templates/js/app.js
+++ templates/js/app.js
@@ -1,23 +1,3 @@
-function getCookie (name) {
-    // Split cookie string and get all individual name=value pairs in an array
-    var cookieArr = document.cookie.split(";");
-    
-    // Loop through the array elements
-    for (var i = 0; i < cookieArr.length; i++) {
-        var cookiePair = cookieArr[i].split("=");
-        
-        /* Removing whitespace at the beginning of the cookie name
-        and compare it with the given string */
-        if (name == cookiePair[0].trim()) {
-            // Decode the cookie value and return
-            return decodeURIComponent(cookiePair[1]);
-        }
-    }
-    
-    // Return null if not found
-    return null;
-}
-
 // Collapsible help dialog
 var coll = document.getElementsByClassName("collapsible");
 for (let i = 0; i < coll.length; i++) {
