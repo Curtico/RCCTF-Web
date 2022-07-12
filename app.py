d01eaa5e5b5240fe29c51aa21a3e8bde93c3856a
Curtice Gough <31411535+Curtico@users.noreply.github.com>
2022-07-12 13:47:28 -0400

    Add login challenge + final touches

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, render_template, request, make_response
 import my_i2c
+import time
 
 app = Flask(__name__, static_url_path="", static_folder="templates")
 
@@ -12,6 +13,16 @@ headlights_on = False
 
 # Encrypted random words so it can't be guessed
 valid_cookies = ["63757274696365", "61584d3d", "59513d3d", "67616d6572"]
+
+@app.route("/robots.txt")
+def robots():
+    response = make_response("User-agent: *\nDisallow: /s3cr3t-p4g3/")
+    response.headers["content-type"] = "text/plain"
+    return response
+
+@app.route("/s3cr3t-p4g3/")
+def secret_page():
+    return render_template('secret-page.html')
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -53,10 +64,10 @@ def index():
 @app.route("/flag-auth/", methods=['POST'])
 def authenticate_flag():
     flags_dictionary = {
-        "forward-movement": "flag{placeholder_1}",
-        "left-movement": "flag{placeholder_2}",
-        "right-movement": "flag{placeholder_3}",
-        "backward-movement": "flag{placeholder_4}"
+        "forward-movement": "flag{w3b_cr4w13r_ru135}",
+        "left-movement": "flag{3ncrypt_y0ur_c00k135}",
+        "right-movement": "flag{p05tm4n_p4t}",
+        "backward-movement": "flag{d0nt_u53_3v41}"
     }
     
     data = request.json;
@@ -82,15 +93,28 @@ def authenticate_flag():
         return "flag_incorrect"
     
 @app.route("/calculator", methods=['GET', 'POST'])
-def post_demo():
+def calculator():
     if request.method == 'GET':
         return render_template('calculator.html')
     else:
         return str(eval(request.json['equation']))
         
-@app.route("/driver")
-def driver_challenge():
-    return render_template('driver-challenge.html')
+@app.route("/flag-generator/", methods=['GET', 'POST'])
+def generator():
+    if request.method == 'GET':
+        return render_template('generator.html')
+    else:
+        return "flag{p05tm4n_p4t}"
+        
+@app.route("/login/")
+def admin():
+    cookie = request.cookies.get('ADMIN')
+    if cookie == 'true':
+        return render_template('admin.html')
+    else:
+        response = make_response(render_template('login.html'))
+        response.set_cookie('ADMIN', 'false')
+        return response
     
 @app.route('/movement/', methods=['POST'])
 def movement():
@@ -143,6 +167,19 @@ def movement():
         my_i2c.lights_off(ic2_bus)
         print("Lights off")
         return "Lights off"
+        
+    elif content['direction'] == "spin":
+        if cookies[1] == valid_cookies[1] and cookies[2] == valid_cookies[2]:
+            my_i2c.turn_left(ic2_bus)
+            time.sleep(0.2)
+            my_i2c.turn_right(ic2_bus)
+            time.sleep(0.2)
+            my_i2c.turn_left(ic2_bus)
+            time.sleep(5)
+            my_i2c.stop(ic2_bus)
+            return "Spin cycle complete!"
+        else:
+            return "Unlock left and right movement to enable the spin cycle"
 
     else:
         return "Bad movement request"
